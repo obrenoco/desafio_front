e62be2047fb6575025bd8036e35e798aa85cf0af
Your Name <bsromeiro@gmail.com>
2020-10-26 16:25:54 -0300

    core | add: solutions component on homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,81 @@
 import React from 'react';
 // import { useDispatch, useSelector } from 'react-redux';
 
-import { Container } from './styles';
+import { Container, Banner, Solutions, Items } from './styles';
 
 // import * as DataActions from '../../store/modules/test/actions';
 
 export default function Home() {
   return (
     <Container>
-      <div>
-        <img alt="Lock Icon" />
+      <Banner>
+        <div>
+          <img alt="Lock Icon" />
+          <h2>Soluções</h2>
+        </div>
+        <p>
+          Na Site Blindado oferecemos vários modelos de certificados para
+          atender à diferentes tipos de negócios e serviços.
+          <br />
+          Veja abaixo qual se encaixa melhor com você.
+          <br />
+          Em caso de dúvidas entre em contato com nossos consultores.
+        </p>
+      </Banner>
+
+      <Solutions>
         <h2>Soluções</h2>
-      </div>
-      <p>
-        Na Site Blindado oferecemos vários modelos de certificados para atender
-        à diferentes tipos de negócios e serviços.
-        <br />
-        Veja abaixo qual se encaixa melhor com você.
-        <br />
-        Em caso de dúvidas entre em contato com nossos consultores.
-      </p>
+        <p>
+          Conheça a suíte de soluções de segurança que a Site Blindado oferece
+        </p>
+        <Items>
+          <div>
+            <img
+              src="https://www.siteblindado.com/images/blindagem.svg"
+              alt="Blindagem de sites"
+            />
+            <strong>Blindagem de sites</strong>
+            <p>
+              Blinde seu site contra invasores e exiba o Selo Site Blindado.
+            </p>
+            <a href="">
+              Conheça melhor <b>>></b>
+            </a>
+          </div>
+          <div>
+            <img
+              src="https://www.siteblindado.com/images/certificado-ssl.svg"
+              alt=""
+            />
+            <strong>Certificados SSL</strong>
+            <p>
+              Proteja as informações em tráfego dos seus clientes e usuários.
+            </p>
+            <a href="">
+              Conheça melhor <b>>></b>
+            </a>
+          </div>
+          <div>
+            <img src="https://www.siteblindado.com/images/waf.svg" alt="" />
+            <strong>WAF</strong>
+            <p>Filtre os tráfegos suspeitos em seu site evitando ataques.</p>
+            <a href="">
+              Conheça melhor <b>>></b>
+            </a>
+          </div>
+          <div>
+            <img src="https://www.siteblindado.com/images/pentest.svg" alt="" />
+            <strong>Pentest</strong>
+            <p>
+              Faça um teste manual de invasão e descubra as falhas do seu site.
+            </p>
+            <a href="">
+              Conheça melhor <b>>></b>
+            </a>
+          </div>
+        </Items>
+        <span></span>
+      </Solutions>
     </Container>
   );
 }
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 import MainBanner from '../../assets/images/banner/banner-blindagem.jpg';
 
-export const Container = styled.div`
+export const Container = styled.div``;
+
+export const Banner = styled.div`
   background-image: url("${MainBanner}");
   background-size: cover;
   background-position: center;
+  background-attachment: fixed;
   height: 32rem;
   display: flex;
   flex-direction: column;
@@ -35,5 +38,61 @@ export const Container = styled.div`
     font-weight: 100;
     margin-top: 1rem;
     max-width: 50vw;
+  }
+`;
+
+export const Solutions = styled.div`
+  text-align: center;
+  padding: 6rem 0;
+  h2 {
+    font-size: 30px;
+    font-weight: 700;
+    margin-bottom: 2rem;
+  }
+
+  p {
+    color: #737a81;
+    font-size: 18px;
+  }
+  span {
+    display: block;
+    height: 0.2rem;
+    width: 36%;
+    background: #74b61b;
+    margin: 5rem auto;
+  }
+`;
+
+export const Items = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 5rem auto;
+  padding: 0 3rem;
+  max-width: 85rem;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 20rem;
+    padding: 0 30px;
+    margin: 0 1rem;
+  }
+
+  strong {
+    font-size: 24px;
+    font-weight: 400;
+    margin: 1rem 0 0.3rem;
+  }
+
+  img {
+    width: 55px;
+  }
+
+  a {
+    margin-top: 1rem;
+    color: #74b61b;
+    font-size: 18px;
   }
 `;
