28ba40cbb8568f94ea4b278d084320181a886f62
Your Name <bsromeiro@gmail.com>
2020-10-26 11:40:30 -0300

    core | add: footer for web

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Row, Input } from './styles';
 
 const Footer = () => {
-  return <Container>Footer</Container>;
+  return (
+    <Container>
+      <Row>
+        <Input>
+          <div>Powered by</div>
+          <a href="https://ri.b2w.digital/">
+            <img
+              src="https://www.siteblindado.com/images/footer/b2w-digital.png"
+              alt=""
+            />
+          </a>
+        </Input>
+        <Input>
+          <a href="https://ri.b2w.digital/">
+            <img
+              src="https://www.siteblindado.com/images/footer/bseller-p&b.png"
+              alt=""
+            />
+          </a>
+        </Input>
+        <Input>
+          <a href="https://ri.b2w.digital/">
+            <img
+              src="https://www.siteblindado.com/images/footer/site-blindado-p&b.png"
+              alt=""
+            />
+          </a>
+        </Input>
+        <Input>
+          <a href="https://ri.b2w.digital/">
+            <img
+              src="https://www.siteblindado.com/images/footer/skyhub-p&b.png"
+              alt=""
+            />
+          </a>
+        </Input>
+        <Input>
+          <a href="https://ri.b2w.digital/">
+            <img
+              src="https://www.siteblindado.com/images/footer/sieve-p&b.png"
+              alt=""
+            />
+          </a>
+        </Input>
+        <Input>
+          <a href="https://ri.b2w.digital/">
+            <img
+              src="https://www.siteblindado.com/images/footer/admatic-p&b.png"
+              alt=""
+            />
+          </a>
+        </Input>
+      </Row>
+    </Container>
+  );
 };
 
 export default Footer;
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,6 +1,43 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  position: absolute;
-  bottom: 0;
+  font-weight: 300;
+  display: flex;
+  align-items: center;
+  background: #13161c;
+  width: 100%;
+  height: 8rem;
+  color: #737a81;
+  text-indent: 2rem;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 120rem;
+`;
+
+export const Input = styled.div`
+  width: 100%;
+  position: relative;
+  :nth-child(1n) {
+    padding: 0.6rem 4rem 0 4rem;
+    flex: 3;
+    max-width: 30rem;
+    div {
+      position: absolute;
+      margin: -2rem 0 0 -1rem;
+    }
+  }
+  :nth-child(n + 2) {
+    padding: 0.6rem 0 0 0;
+    display: flex;
+    flex-direction: column;
+    flex-basis: 100%;
+    flex: 1;
+    max-width: 40rem;
+    align-self: center;
+  }
 `;
