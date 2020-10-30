2dd2ed8175fc259ed4aa883a4df624c6c1f09c14
Your Name <bsromeiro@gmail.com>
2020-10-30 15:28:32 -0300

    core | add: api call & formatted data on charts page

--- src/pages/Dashboard/components/Chart/index.tsx
+++ src/pages/Dashboard/components/Chart/index.tsx
@@ -1,10 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MUIDataTable from 'mui-datatables';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
 
+import api from 'services/MockedApi';
+
+import formatDataCharts from 'utils/formatedDataCharts';
 import { Container } from './styles';
 
+interface ValuesProps {
+  data: {
+    dataCharts: {
+      monthlyContractOverdue: {
+        labels: string[];
+        series: number[][];
+        totalOverdueValue: number;
+      };
+      totalPaid: {
+        labels: string[];
+        series: number[][];
+        totalOverdueValue: number;
+      };
+      totalSold: {
+        labels: string[];
+        series: number[][];
+        totalOverdueValue: number;
+      };
+    };
+    reference: {
+      referenceMonth: 'string';
+      referenceYear: 'string';
+    };
+  };
+}
+
 const Chart: React.FC = () => {
+  const [value, setValues] = useState<ValuesProps>();
+
+  useEffect(() => {
+    api.get('').then(response => {
+      setValues(response.data);
+    });
+  }, []);
+
+  const months = [
+    '1.Janeiro',
+    '2.Fevereiro',
+    '3.Março',
+    '4.Abril',
+    '5.Maio',
+    '6.Junho',
+    '7.Julho',
+    '8.Agosto',
+    '9.Setembro',
+    '10.Outubro',
+    '11.Novembro',
+    '12.Dezembro',
+  ];
+
+  const monthlyContractOverdue =
+    value?.data.dataCharts.monthlyContractOverdue.series[0];
+  const totalPaid = value?.data.dataCharts.totalPaid.series[0];
+  const totalSold = value?.data.dataCharts.totalSold.series[0];
+
+  const formattedTotalPaid = totalPaid?.map(formatDataCharts);
+  const formattedTotalSold = totalSold?.map(formatDataCharts);
+
+  console.log(monthlyContractOverdue);
+  console.log(formattedTotalPaid);
+  console.log(formattedTotalSold);
+  console.log(months);
+
   const data = [
     [5, 'R$ 590', 'R$ 350', '1.Janeiro'],
     [6, 'R$ 664', 'R$ 250', '1.Janeiro'],
@@ -37,20 +102,7 @@ const Chart: React.FC = () => {
       options: {
         filter: true,
         filterOptions: {
-          names: [
-            '1.Janeiro',
-            '2.Fevereiro',
-            '3.Março',
-            '4.Abril',
-            '5.Maio',
-            '6.Junho',
-            '7.Julho',
-            '8.Agosto',
-            '9.Setembro',
-            '10.Outubro',
-            '11.Novembro',
-            '12.Dezembro',
-          ],
+          names: months,
         },
       },
     },
@@ -71,7 +123,7 @@ const Chart: React.FC = () => {
     <Container>
       <MuiThemeProvider theme={theme}>
         <MUIDataTable
-          title={'Dezembro 2020'}
+          title={`${value?.data.reference.referenceMonth} ${value?.data.reference.referenceYear}`}
           data={data}
           columns={columns}
           options={options}
---
+++ src/utils/formatedDataCharts.ts
@@ -0,0 +1,9 @@
+const formatDataCharts = (value: number): string =>
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(value);
+
+export default formatDataCharts;
